1344d3d6fb9355e90a52731dfda218ae6a0ec35d
62160050 <75073803+62160050@users.noreply.github.com>
2021-03-27 16:37:27 +0700

    edit sql

--- 62160050_miniproject/real-time-chat/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id int(11) AUTO_INCREMENT PRIMARY KEY,
-    name varchar(50) not null,
-    email varchar(100) not null UNIQUE KEY
-)ENGINE = INNODB DEFAULT charset=utf8;
-
-INSERT INTO users (ID, name, email) VALUES ('', 'kit', 'Aukit@gmail.com'), ('', 'mail', 'mailza@gmail.com'), ('', 'maprang', 'maprang@gmail.com');
